104b424900b515e4315602d793cdd486598a47ff
andreas.dominik <andreas.dominik@mni.thm.de>
2020-03-22 10:31:53 +0100

    template adapted to language handling

--- Skill/config.jl
+++ Skill/config.jl
@@ -1,23 +1,37 @@
-
-# language settings:
-# 1) set LANG to "en", "de", "fr", etc.
-# 2) link the Dict with messages to the version with
-#    desired language as defined in languages.jl:
-#
-
-lang = Snips.getConfig(:language)
-const LANG = (lang != nothing) ? lang : "de"
-
-# DO NOT CHANGE THE FOLLOWING 3 LINES UNLESS YOU KNOW
+# DO NOT CHANGE THE FOLLOWING LINES UNLESS YOU KNOW
 # WHAT YOU ARE DOING!
 # set CONTINUE_WO_HOTWORD to true to be able to chain
 # commands without need of a hotword in between:
 #
-const CONTINUE_WO_HOTWORD = true
+const CONTINUE_WO_HOTWORD = false
 const DEVELOPER_NAME = "andreasdominik"
 Snips.setDeveloperName(DEVELOPER_NAME)
 Snips.setModule(@__MODULE__)
+#
+# language settings:
+# Snips.LANG in QnD(Snips) is defined from susi.toml or set
+# to "en" if no susi.toml found.
+# This will override LANG by config.ini if a key "language"
+# is defined locally:
+#
+if Snips.isConfigValid(:language)
+    Snips.setLanguage(Snips.getConfig(:language))
+end
+# or LANG can be set manually here:
+# Snips.setLanguage("fr")
+#
+# set a local const with LANG:
+#
+const LANG = Snips.getLanguage()
+#
+# END OF DO-NOT-CHANGE.
 
+
+# ************************************************************
+# INSERT HERE THE DEFINITIONS OF SLOTS AND CONFIG.INI ENTRIES:
+# ************************************************************
+#
+#
 # Slots:
 # Name of slots to be extracted from intents:
 #
@@ -27,8 +41,9 @@ const SLOT_WORD = "a_word"
 #
 const INI_MY_NAME = "my_name"
 
+
+# INSERT HERE LINKS BETWEEN ACTIONS AND INTENTS:
 #
-# link between actions and intents:
 # intent is linked to action{Funktion}
 # the action is only matched, if
 #   * intentname matches and
